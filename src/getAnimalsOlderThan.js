e5ff23deea5a67d38d9a3036f41523d4bf0fca82
Arthur <arthurbarbosa93@gmail.com>
2022-01-25 17:20:20 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+/* Uso o método find para verificar o nome de entrada do animal, após isso faço a condição com a idade la no residents pelo every */
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const bicho = data.species.find((element) => element.name === animal);
+
+  return bicho.residents.every((idade) => idade.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
