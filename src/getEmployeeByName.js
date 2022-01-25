995a6a7c6c0f0ec8497dd0636e2942334e4cbf50
Arthur <arthurbarbosa93@gmail.com>
2022-01-25 18:25:25 -0300

    Corrigindo

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,15 +1,13 @@
-/* eslint-disable no-unreachable */
 const data = require('../data/zoo_data');
-
 /* Usei correção rápida do lint por max de caract... na linha */
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
-  // eslint-disable-next-line max-len
-  const empregados = data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
-  return empregados;
+  return data.employees
+    .find((element) => element.firstName === employeeName || element.lastName
+     === employeeName);
 }
 
 module.exports = getEmployeeByName;
