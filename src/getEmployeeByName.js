12ddab3f328038a9a04a93d02acb6a7965c4f2ed
Arthur <arthurbarbosa93@gmail.com>
2022-01-25 17:43:22 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-unreachable */
 const data = require('../data/zoo_data');
 
+/* Usei correção rápida do lint por max de caract... na linha */
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  // eslint-disable-next-line max-len
+  const empregados = data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
+  return empregados;
 }
 
 module.exports = getEmployeeByName;
