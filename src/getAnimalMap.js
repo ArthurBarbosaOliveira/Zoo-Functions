25fda139f3d38ca0fb672d356a9e32515d92912e
Arthur <arthurbarbosa93@gmail.com>
2022-02-02 14:01:16 -0300

    Lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -3,13 +3,11 @@ const data = require('../data/zoo_data');
 function getAnimalMap(options) {
   if (options === undefined) {
     return {
-      NE: data.species.filter((element) => element.location === 'NE') 
-      NW: 0,
+      NE: data.species.filter((element) => element.location === 'NE'),
       SE: 0,
       SW: 0,
     };
-
-}
+  }
 }
 
 module.exports = getAnimalMap;
