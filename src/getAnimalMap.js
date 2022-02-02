0602ba1c8c8d721e355c50851d58073898e75acd
Arthur <arthurbarbosa93@gmail.com>
2022-02-02 13:22:20 -0300

    Requisito 9

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    return {
+      NE: data.species.filter((element) => element.location === 'NE') 
+      NW: 0,
+      SE: 0,
+      SW: 0,
+    };
+
+}
 }
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { employess, species } = data;
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+  /* vamos pegar o valor do array employees que satisfaça minha condição e guardar ela.
+   to verificando o id do funcionario passado como parametro para encontrar
+   com === id e responsibleFor[0] o primeiro animal gerenciado pelo funcionario */
+  const funcionarios = employess.find((element) => element.id === id).responsibleFor[0];
+  console.log(funcionarios); 
+  /* irie usar o find para pegar todas informações do residents que satisfaça minha condição e guaradr na const como um array */
+  const informações = species.find((element) => element.id === funcionarios).residents;
+  /* e aqui faço o reduce com callback, aonde irei atualizar o valor a cada interação, passando pela condição com o ternaty operator */
+  const bichos = informações.reduce((acc, curr ) => (acc.age > curr.age ? acc : curr ));
+  /* ele espera um objeto com os dados no resultado */
+  return Object.values(bichos);
+};
 
 module.exports = getOldestFromFirstSpecies;
